f9f4844f187cc3574dca553def0e6133eecb5f39
bigbadabum6 <dolgkozly@gmail.com>
2022-07-31 17:54:39 +0300

    Last commit

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,12 @@
-const volume =
-  document.querySelector(".volume");
+function volume() {
+  const radius =
+    document.getElementById(
+      "name"
+    ).value;
+  const result = Math.pow(radius, 3);
+
+  let volume = (4 / 3) * 3.14 * result;
+  const htmlElement =
+    document.querySelector("#volume");
+  htmlElement.textContent = volume;
+}
